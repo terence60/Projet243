69cd7b8e9d6e6fb87685f226663165c1cbceda63
Terence Dinguilu <terence.dinguilu@gmail.com>
2025-08-13 17:12:09 +0200

    entity

---
+++ src/Entity/Photo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoRepository::class)]
+class Photo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
