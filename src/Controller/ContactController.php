2c671fca64876faa462473b0efc78a541f53f2ba
Terence Dinguilu <terence.dinguilu@gmail.com>
2025-08-25 16:41:01 +0200

    form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactForm;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request,MailerInterface $mailer): Response
     {
+       $form = $this->createForm(ContactForm::class);
+
+       $form->handleRequest($request);
+
+       if($form->isSubmitted()&& $form->isValid()){
+
+        $data = $form->getData();
+        $address = $data['email'];
+        $message = $data['message'];
+
+        $email = (new Email())
+
+            ->from($address)
+            ->to('tete@blog.com')
+            ->subject('Demande de contact')
+            ->text($message);
+
+            $mailer->send($email);
+       }
+
         return $this->render('contact/index.html.twig', [
-            
+        'formulaire' => $form
         ]);
     }
 }
